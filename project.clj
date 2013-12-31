75ad71a590ba9f1a5246ed148cbb2ed72e3b4824
ruttenberg <ruttenberg@stanfordalumni.org>
2013-12-31 12:38:19 -0500

    fix URL MIT license

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject monitor-rabbitmq "0.1.0-SNAPSHOT"
   :description "query RabbitMQ management API for queue statistics and send them to Riemann"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :url "https://github.com/TheLadders/monitor-rabbitmq"
+  :license {:name "The MIT License (MIT)"
+            :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [cheshire "5.2.0"]
                  [clj-http "0.7.8"]
