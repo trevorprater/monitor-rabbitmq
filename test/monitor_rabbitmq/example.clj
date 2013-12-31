4b363a113f0f2ca37400342e7ced855d29ae368c
ruttenberg <ruttenberg@stanfordalumni.org>
2013-12-31 13:35:18 -0500

    sample usage of library

---
+++ test/monitor_rabbitmq/example.clj
@@ -0,0 +1,20 @@
+(ns monitor-rabbitmq.example
+  (:require [monitor-rabbitmq.core :as monitor]))
+
+(def rmq "my-rabbitmq.net:4321" )
+(def r-user "monitoring" )
+(def r-pass  "easy-pass" )
+(def rmq-display-name  "rabbit-mq-qa" )
+(def Riemann-host "my-riemann.net")
+(def age-of-oldest-sample-in-seconds 300)
+(def seconds-between-samples 15)
+
+(defn do-it []
+  (monitor/send-rabbitmq-stats-to-Riemann rmq
+                                          r-user
+                                          r-pass
+                                          age-of-oldest-sample-in-seconds
+                                          seconds-between-samples
+                                          rmq-display-name
+                                          Riemann-host)
+  )
